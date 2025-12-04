ba9c01ac8cc552a3ac79851d7cf0ebc5da239088
cdn-lgn <logan2246t@gmail.com>
2025-12-04 06:47:01 +0530

    DEC 4 25 setup for c language and first program create

---
+++ chapter-1/first.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+// stdio.h ==> standard input output header file included
+// int main() ==> function declaration. must declare
+// printf ==> inbuild function of stdio
+// ; ==> line completed
+// return 0; ==> success
+
+int main()
+{
+  printf("My Age is %d\n",32);
+  printf("Character %c\n",'A');
+  printf("Decimal %f\n",34.0);
+  return 0;
+}
+
+
+// %d or %i for integers
+// %c for characters
+// %f for decimal numbers
+// \n for new line
+
+
+
+//==> importance of main method <==
+// It is the entry point of a C program, where the execution starts.
+// When a C program is executed, the control starts from the main function.
+// Typically returns int, indication program success(0) or error (non-zero) to the operating system.
+// The name main is recognized by C compilers as the program's starting point.
+
+
+
+// ==> File Extension <==
+// .C :=>
+// Containe Executable code.
+// Compiled: Transformed into executable programs.
+// Often hosts the main.
+// .h :=>
+// Contain Declarations for code to be shared.
+// Improve modularity and managebility in projects.
+// Facilitate code use across multiple .c files without duplication.
+
+
+
+
+// ==> Comments <==
+// Used to add notes in C code.
+// Note considered as part of code.
+// Helpful for code organization.
+// Syntax :
+//               Single line //
+//               multi line /* */
+
+
+// 1 hour watched
